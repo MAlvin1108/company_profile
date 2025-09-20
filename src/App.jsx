44d342ffb044d8047bf703c55aa99b8ac82dc766
unknown <alvinmaulana949@gmail.com>
2025-09-20 15:33:01 +0700

    new progress

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./view/landing-page/landing_view.jsx";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/view/landing-page/landing_view.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./landing.css";
+
+/*Model*/
+
+const LandingPage = () => {
+  return (
+    <div class="container-section">
+      <section class="section-1">
+        <h1>WELCOME TO INDOLOCAL</h1>
+        <p>we're the biggest electrical motocycle distribution</p>
+      </section>
+      <section class="section-2">
+        <h2>Product</h2>
+        <div class="card-expose">
+          <h3></h3>
+          <img src="" alt="product1" />
+          <p></p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default LandingPage;
