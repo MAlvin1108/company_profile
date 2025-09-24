3a98162d8fe0d19069f0dc76b9730da306ab3d0f
unknown <alvinmaulana949@gmail.com>
2025-09-24 21:19:19 +0700

    latest update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const AppWrapper = () => {
+  useEffect(() => {
+    if (window.feather) {
+      window.feather.replace();
+    }
+  }, []);
+
+  return <App />;
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppWrapper />
+  </StrictMode>
+);
